1ef8e41c5d6b82d8abe18127e52f2892fd9e4060
cp-24lar <louisarmand.rossier@ceff.ch>
2026-06-30 11:31:59 +0200

    a

--- scripts/al_colique.gd
+++ scripts/al_colique.gd
@@ -5,22 +5,24 @@ var direction = -1
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
-	if $RayCastLeft.is_colliding() || $Left/RayCastLeft.is_colliding():
-		$Left.visible = false
-		$KillZone/LR.disabled = true
-		$KillZone/LL.disabled = true
-		$KillZone/RL.disabled = false
-		$KillZone/RR.disabled = false
-		$Right.visible = true
+	if $RayCastLeft.is_colliding():
+		$CharacterContainer/Body.rotate(180)
+		#$Left.visible = false
+		#$KillZone/LR.disabled = true
+		#$KillZone/LL.disabled = true
+		#$KillZone/RL.disabled = false
+		#$KillZone/RR.disabled = false
+		#$Right.visible = true
 		direction = 1
 		print("test")
-	if $RayCastRight.is_colliding() || $Left/RayCastRight.is_colliding():
-		$Left.visible = true
-		$KillZone/LR.disabled = false
-		$KillZone/LL.disabled = false
-		$KillZone/RL.disabled = true
-		$KillZone/RR.disabled = true
-		$Right.visible = false
+	if $RayCastRight.is_colliding():
+		$CharacterContainer/Body.rotate(180)
+		#$Left.visible = true
+		#$KillZone/LR.disabled = false
+		#$KillZone/LL.disabled = false
+		#$KillZone/RL.disabled = true
+		#$KillZone/RR.disabled = true
+		#$Right.visible = false
 		direction = -1
 		print("test")
 	position.x += direction * SPEED * delta
