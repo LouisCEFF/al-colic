5b6051783b839f802f1adcbf231c8d72f095ae73
cp-24lar <louisarmand.rossier@ceff.ch>
2026-06-30 11:03:28 +0200

    Yes

--- scripts/al_colique.gd
+++ scripts/al_colique.gd
@@ -1,17 +1,26 @@
-extends CharacterBody2D
+extends Node2D
 
-@export var SPEED = 60
+@export var SPEED = 90
 var direction = -1
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	if $RayCastLeft.is_colliding():
-		$Sprite2D.flip_h = true
+func _physics_process(delta: float) -> void:
+	if $RayCastLeft.is_colliding() || $Left/RayCastLeft.is_colliding():
+		$Left.visible = false
+		$KillZone/LR.disabled = true
+		$KillZone/LL.disabled = true
+		$KillZone/RL.disabled = false
+		$KillZone/RR.disabled = false
+		$Right.visible = true
 		direction = 1
 		print("test")
-	if $RayCastRight.is_colliding():
-		$Sprite2D.flip_h = false
+	if $RayCastRight.is_colliding() || $Left/RayCastRight.is_colliding():
+		$Left.visible = true
+		$KillZone/LR.disabled = false
+		$KillZone/LL.disabled = false
+		$KillZone/RL.disabled = true
+		$KillZone/RR.disabled = true
+		$Right.visible = false
 		direction = -1
 		print("test")
 	position.x += direction * SPEED * delta
